7039299cb0c0b039ed657d0dc122c11b7d72c386
Chris <chris@tonzof.com>
2014-07-24 16:34:53 -0600

    Adding changes to HttpHelper
    - Using empty() instead of != null as its faster ()
    - Checking if class property exists before using it to prevent PHP
    Notices
    - Docs
    - Commented out switch cases that have no logic, now they are defaulting

--- src/Mozu/Api/Utilities/HttpHelper.php
+++ src/Mozu/Api/Utilities/HttpHelper.php
@@ -5,51 +5,125 @@ use Mozu\Api\Utilities\Proxy;
 use Mozu\Api\ApiException;
 use Mozu\Api\iApiContext;
 
+/**
+ * HttpHelper
+ *
+ * This static class attempts to translate exceptions into something easier for the developer to comprehend
+ *
+ * <Chris Childress>
+ *
+ * Jul 24, 2014
+ */
 class HttpHelper {
 
-	public static $urlScheme = "http";
-	
-	public static function getGuzzleConfig() {
-		$proxy = Proxy::getInstance();
-		$config = null;
-		if (isset($proxy))
-			$config = array('version' => 'v1.1','curl.options' => array(CURLOPT_PROXY => $proxy->getHost() . ":" . $proxy->getPort()));
-		
-		return $config;
-	}
-	
-	public static function checkError(\Exception $e, iApiContext $apiContext = null) {
-		switch(get_class($e)) {
-			case "Guzzle\Http\Exception\BadResponseException":
-			case "Guzzle\Http\Exception\ClientErrorResponseException":
-			case "Guzzle\Http\Exception\ServerErrorResponseException":
-				//var_dump ($e);
-				$resp = $e->getResponse()->getBody ( true );
-				$jsonResponse = json_decode ( $resp );
-				$message = (get_class($jsonResponse) == "stdClass" ? $jsonResponse->message : $e->getResponse()->getReasonPhrase());
-				$apiException = new ApiException($message, $e->getResponse()->getStatusCode());
-				$header = (string)$e->getResponse()->getHeader("x-vol-correlation");
-				
-				if ($header != null) {
-					$apiException->setCorrelationId($header);
-				}
-				$apiException->setApiContext($apiContext);
-				if ($jsonResponse != null) {
-					$apiException->setAdditionalErrorData($jsonResponse->additionalErrorData);
-					$apiException->setErrorCode($jsonResponse->errorCode);
-					$apiException->setApplicationName($jsonResponse->applicationName);
-					$apiException->setItems($jsonResponse->items);
-				}
-				throw $apiException;
-				break;
-			default:
-				throw $e;
-		}
-	}
-	
-	public static function getUrl($url) {
-		return static::$urlScheme."://".$url;
-	}
+    /**
+     *
+     * @var string $urlScheme
+     */
+    public static $urlScheme = "http";
+
+    /**
+     * getGuzzleConfig
+     *
+     * Used to return our current Guzzle configuration
+     *
+     * @return Ambigous <NULL, multitype:string multitype:string >
+     */
+    public static function getGuzzleConfig() {
+        $proxy = Proxy::getInstance();
+        $config = null;
+        if (isset($proxy)) $config = array(
+            'version' => 'v1.1',
+            'curl.options' => array(
+                CURLOPT_PROXY => $proxy->getHost() . ":" . $proxy->getPort()
+            )
+        );
+        
+        return $config;
+    }
+
+    /**
+     * checkError
+     *
+     * This method will attempt to translate error messages from Guzzle
+     *
+     * @param \Exception $e            
+     * @param iApiContext $apiContext            
+     * @throws \Mozu\Api\ApiException
+     * @throws ApiException
+     */
+    public static function checkError(\Exception $e, iApiContext $apiContext = null) {
+        switch (get_class($e)) {
+            
+            // Commenting out because Mozu hasn't built logic for this yet. Now this case will default instead of calling Guzzle\Http\Exception\ServerErrorResponseException
+            // case "Guzzle\Http\Exception\BadResponseException":
+            
+            case "Guzzle\Http\Exception\ClientErrorResponseException":
+                $message = $e->getResponse()->getReasonPhrase();
+                $code = $e->getResponse()->getStatusCode();
+                $response = $e->getResponse();
+                $jsonResponse = json_decode($response);
+                if (empty($message) || empty($code)) {
+                    throw $e;
+                }
+                $apiException = new ApiException($message, $code);
+                $header = (string)$e->getResponse()->getHeader("x-vol-correlation");
+                if (!empty($header)) {
+                    $apiException->setCorrelationId($header);
+                }
+                if (!empty($jsonResponse)) {
+                    if (isset($jsonResponse->additionalErrorData)) {
+                        $apiException->setAdditionalErrorData($jsonResponse->additionalErrorData);
+                    }
+                    if (isset($jsonResponse->errorCode)) {
+                        $apiException->setErrorCode($jsonResponse->errorCode);
+                    }
+                    if (isset($jsonResponse->applicationName)) {
+                        $apiException->setApplicationName($jsonResponse->applicationName);
+                    }
+                    if (isset($jsonResponse->items)) {
+                        $apiException->setItems($jsonResponse->items);
+                    }
+                }
+                throw $apiException;
+                break;
+            
+            case "Guzzle\Http\Exception\ServerErrorResponseException":
+                // var_dump ($e);
+                $resp = $e->getResponse()->getBody(true);
+                $jsonResponse = json_decode($resp);
+                $message = (get_class($jsonResponse) == "stdClass" ? $jsonResponse->message : $e->getResponse()->getReasonPhrase());
+                $apiException = new ApiException($message, $e->getResponse()->getStatusCode());
+                $header = (string)$e->getResponse()->getHeader("x-vol-correlation");
+                
+                if ($header != null) {
+                    $apiException->setCorrelationId($header);
+                }
+                $apiException->setApiContext($apiContext);
+                if ($jsonResponse != null) {
+                    $apiException->setAdditionalErrorData($jsonResponse->additionalErrorData);
+                    $apiException->setErrorCode($jsonResponse->errorCode);
+                    $apiException->setApplicationName($jsonResponse->applicationName);
+                    $apiException->setItems($jsonResponse->items);
+                }
+                throw $apiException;
+                break;
+            default:
+                throw $e;
+        }
+    }
+
+    /**
+     * getUrl
+     *
+     * This method builds our current url based on the configured $urlScheme
+     *
+     * @param string $url            
+     * @return string
+     */
+    public static function getUrl($url) {
+        return static::$urlScheme . "://" . $url;
+    }
 }
 
 ?>
