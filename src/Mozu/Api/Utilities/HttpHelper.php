0d82b4932ca4cf345a0dc64c2f599383a745a072
Chris <chris@tonzof.com>
2014-07-24 17:51:35 -0600

    Using fallthrough in the switch statement.

--- src/Mozu/Api/Utilities/HttpHelper.php
+++ src/Mozu/Api/Utilities/HttpHelper.php
@@ -54,47 +54,15 @@ class HttpHelper {
      */
     public static function checkError(\Exception $e, iApiContext $apiContext = null) {
         switch (get_class($e)) {
-            
-            // Commenting out because Mozu hasn't built logic for this yet. Now this case will default instead of calling Guzzle\Http\Exception\ServerErrorResponseException
-            // case "Guzzle\Http\Exception\BadResponseException":
-            
+            case "Guzzle\Http\Exception\BadResponseException":
             case "Guzzle\Http\Exception\ClientErrorResponseException":
-                $message = $e->getResponse()->getReasonPhrase();
-                $code = $e->getResponse()->getStatusCode();
-                $response = $e->getResponse(TRUE);
-                $jsonResponse = json_decode($response);
-                if (empty($message) || empty($code)) {
-                    throw $e;
-                }
-                $apiException = new ApiException($message, $code);
-                $header = (string)$e->getResponse()->getHeader("x-vol-correlation");
-                if (!empty($header)) {
-                    $apiException->setCorrelationId($header);
-                }
-                if (!empty($jsonResponse)) {
-                    if (isset($jsonResponse->additionalErrorData)) {
-                        $apiException->setAdditionalErrorData($jsonResponse->additionalErrorData);
-                    }
-                    if (isset($jsonResponse->errorCode)) {
-                        $apiException->setErrorCode($jsonResponse->errorCode);
-                    }
-                    if (isset($jsonResponse->applicationName)) {
-                        $apiException->setApplicationName($jsonResponse->applicationName);
-                    }
-                    if (isset($jsonResponse->items)) {
-                        $apiException->setItems($jsonResponse->items);
-                    }
-                }
-                throw $apiException;
-                break;
-            
             case "Guzzle\Http\Exception\ServerErrorResponseException":
                 // var_dump ($e);
                 $code = $e->getResponse()->getStatusCode();
                 $response = $e->getResponse()->getBody(TRUE);
                 $jsonResponse = json_decode($response);
                 $message = $e->getResponse()->getReasonPhrase();
-                $apiException = new ApiException($message, $e->getResponse()->getStatusCode());
+                $apiException = new ApiException($message, $code);
                 $header = (string)$e->getResponse()->getHeader("x-vol-correlation");
                 if (!empty($header)) {
                     $apiException->setCorrelationId($header);
